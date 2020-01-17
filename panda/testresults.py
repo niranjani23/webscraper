6c7dfe064e6e422c9271dcac0e09364c731a0cee
Niranjani <nmanoharan@lyft.com>
2020-01-16 18:09:55 -0800

    Update mongodb insertion

--- panda/testresults.py
+++ panda/testresults.py
@@ -4,15 +4,13 @@ import matplotlib.pyplot as plt
 # collect data for Lyft from 2019-05-22 to 2020-01-01
 start = '2019-04-22'
 end = '2020-01-01'
-symbol='LYFT'
-df = web.DataReader(name=symbol, data_source='iex', start=start, end=end)
-#df.index = df.index.to_datetime()
-print(df)
-df.to_csv("~/workspace/{}.csv".format("stock"))
-# select only close column
-close = df[['close']]
+symbol='LYFT DRIVER'
+
+#Read from csv file
+
+
 # rename the column with symbol name
-close = close.rename(columns={'close': "stock"})
+#close = close.rename(columns={'close': "stock"})
 ax = close.plot(title='Lyft')
 ax.set_xlabel('date')
 ax.set_ylabel('close price')
--- ticker/tickerdata.py
+++ ticker/tickerdata.py
@@ -7,7 +7,7 @@ API_KEY = os.environ['WORLD_TRADING_DATA_API_KEY']
 
 
 def get_data():
-    resp = requests.get("https://api.worldtradingdata.com/api/v1/stock?symbol=AAPL&api_token=%s" % API_KEY)
+    resp = requests.get("https://api.worldtradingdata.com/api/v1/stock?symbol=LYFT&api_token=%s" % API_KEY)
     print(resp.text)
     data = json.loads(resp.text)
     print(data)
@@ -15,41 +15,8 @@ def get_data():
     mydb = myclient["mystockdb"]
     mycol = mydb["tickerdata"]
     mydict = data
-    mydict2 = {
-      "data": [
-        {
-          "52_week_high": "310.43",
-          "52_week_low": "149.22",
-          "change_pct": "2.12",
-          "close_yesterday": "303.19",
-          "currency": "USD",
-          "day_change": "6.44",
-          "day_high": "310.43",
-          "day_low": "306.20",
-          "eps": "11.89",
-          "gmt_offset": "-18000",
-          "last_trade_time": "2020-01-09 16:00:01",
-          "market_cap": "1372775645184",
-          "name": "Apple Inc.",
-          "pe": "26.04",
-          "price": "309.63",
-          "price_open": "307.24",
-          "shares": "4384030208",
-          "stock_exchange_long": "NASDAQ Stock Exchange",
-          "stock_exchange_short": "NASDAQ",
-          "symbol": "AAPL",
-          "timezone": "EST",
-          "timezone_name": "America/New_York",
-          "volume": "39450557",
-          "volume_avg": "31645057"
-        }
-      ],
-      "symbols_requested": 1,
-      "symbols_returned": 1
-    }
     print('*********')
-    x = mycol.insert_one(mydict)
-    #x = mycol.insert(mydict)
+    x = mycol.insert(mydict, manipulate=False)
     print(mydb.list_collection_names())
     return data
 
